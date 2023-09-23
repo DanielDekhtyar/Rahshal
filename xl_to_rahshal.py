2b9e4f04a0050bdf2bbd673449ae9dafe5784b54
Daniel Dekhtyar <denik2707@gmail.com>
2023-09-24 00:23:03 +0300

    is_area_in_rahshal() implemented

--- xl_to_rahshal.py
+++ xl_to_rahshal.py
@@ -13,7 +13,9 @@ def main():
     while xl_row < nz_xl.max_row:
         area_name, xl_row = find_area_in_xl(nz_xl, xl_row)
         if area_name != " ":
-            print(area_name)
+            results = is_area_in_rahshal(area_name, rahshal)
+            if results is not None: # Check if None is returned, meaning that the area is not in the docx
+                area_name, paragraph = results
         
     print(f"--- The code took {time.time() - start_time} seconds to run ---")
 
@@ -25,6 +27,19 @@ def find_area_in_xl(nz_xl, xl_row): # TESTED AND DONE !
             if "קורדינטות" not in cell_value:
                 return cell_value, row
     return " ", row
+
+
+def is_area_in_rahshal(area_name, rahshal):
+    for paragraph in enumerate(rahshal.paragraphs):
+        if area_name == paragraph[1].text:
+            results = area_name, paragraph
+            return results
+        
+    print(f"The area {area_name[::-1]} is not in the docx") # If area is not in rahshal, an error message will appear
+    return None
+# 'TypeError: cannot unpack non-iterable NoneType object' solved by putting all the return values in to one tuple
+# If the function didn't do it's work then return this one variable as None
+# At the receiving end, check if the return value is None, else you can safely unpack the tuple and use it.
 
 if __name__ == "__main__":
     main()
